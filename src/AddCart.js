2b7bfd91b868c1306ae1529e56ce36db025e56c7
Alejandro <89873076+Saenz-77@users.noreply.github.com>
2023-09-09 02:10:45 +0000

    boton whatsapp integrado

--- src/AddCart.js
+++ src/AddCart.js
@@ -3,10 +3,12 @@ import React from 'react';
 function AddCart() {
   return (
     <React.Fragment>
-      <button>
-        <div className='iconWhatsapp'></div>
-        Pedir
-      </button>
+      <a href='https://wa.link/37v0nm'>
+        <button >
+          <div className='iconWhatsapp'></div>
+          Pedir
+        </button>
+      </a>
     </React.Fragment>
     
   )
