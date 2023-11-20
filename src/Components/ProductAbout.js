79117fe6d608105fe2ec8df176aea2fa2c71dda9
Saenz-77 <saenzgarzonalejo@gmail.com>
2023-11-20 14:53:27 -0500

    modificación, espacio segunda imagen productos

--- src/Components/ProductAbout.js
+++ src/Components/ProductAbout.js
@@ -4,7 +4,30 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { AddCart } from "./AddCart";
 
+import second from "../assets/second.jpg"
+
+const imageList = {
+  '../assets/adidas.jpg': second,
+  '../assets/albertofermani.jpg': second,
+  '../assets/caterpillar.jpg': second,
+  '../assets/converse.jpg': second,
+  '../assets/gucci.jpg': second,
+  '../assets/jordan.jpg': second,
+  '../assets/newbalance.jpg': second,
+  '../assets/nike.jpg': second,
+  '../assets/puma.jpg': second,
+  '../assets/reebok.jpg': second,
+  '../assets/skechers.jpg': second,
+  '../assets/underarmor.jpg': second,
+}
+
+function additionalImages(imageMain, imageList) {
+  if (imageList.hasOwnProperty(imageMain)) {
+    return imageList[imageMain];
+  }
+}
 function ProductAbout(props) {
+  console.log(props.imagen)
   const settings = {
     dots: true,
     slidesToShow: 1,
@@ -21,7 +44,7 @@ function ProductAbout(props) {
           </div>
             <Slider {...settings}>
               <img src={props.imagen} alt='producto' className='imageProductAbout'/>
-              <img src={props.imagen} alt='producto' className='imageProductAbout'/>
+              {additionalImages(props.imagen, imageList) && <img src= {additionalImages(props.imagen, imageList)} alt='producto' className='imageProductAbout'/>}
             </Slider>
 
             < AddCart key={props.id} id={props.id} />
